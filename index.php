c29a25b16b34e76174065af04956c0d6b703e812
enjarai <enjarai123@gmail.com>
2022-03-10 10:16:58 +0100

    ayo gefixt :dab:

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <h1>header this</h1>
 
 <?php
-$path = $_GET['path'];
-include $path ?? 'home.php';
-
+$home_page = 'home.php';
+include key_exists('path', $_GET) ? $_GET['path'] != "" ? $_GET['path'] : $home_page : $home_page;
