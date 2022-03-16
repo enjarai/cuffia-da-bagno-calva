641163e3c1f68d2233c65da45521436e50500c5b
kilian2705 <182624@novacollege.nl>
2022-03-16 12:59:45 +0100

    contact-pagina, Kilian

--- contact.php
+++ contact.php
@@ -1,1 +1,29 @@
-contact
+<link rel='stylesheet' href='index.css' type='text/css'>
+
+<div class="items">
+    <div class="item1">
+        <h2>CONTACT</h2>
+    </div>
+    <div class="item2">
+        <h4>Benvenuti ospiti</h4>
+    <p>cuffia da bagno calva biedt de prachtige stranden van Maastricht aan.<br>Voor de kids is er een ballenbak waar zij lekker kunnen spelen met ballen!<br>
+        Er zijn diverse snacks verkrijgbaar.</p>
+    <p>Cuffia da Bagno Calva verzorgt ook graag uw bruiloft, feest, crematie groot of klein!
+        Binnen Cuffia da Bagno Calva hebben we veel ervaring met het organiseren van de ceremonie,
+        receptie, diner en feestavond!<br> Bel of komt u even langs wij vertellen u graag de mogelijkheden.
+        U kunt er verzekerd van zijn dat wij er alles aan doen om van uw bruiloft een succes te maken.</p><p>openingstijden ma - do 09:00 - 21:00 vr - zo 9:00 - 22:00</p>
+    <p>Tot snel bij Cuffia da bagno calva!</p>
+        <img class="item2foto" src="images/contactmannetje.png">
+    </div>
+    <div class="item3">
+        <h2>Contactinfo</h2>
+        <hr>
+        <p>Snackbar Cuffia da Bagno Calva<br>Annie Romeinstraat 28<br>5803 AE Maastricht</p>
+        <p>Telefoon: 023 515 9500<br>E-mail: info@cuffia-da-bagno-calva</p>
+        <div class="mapouter">
+            <div class="gmap_canvas">
+                <iframe width="450" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Annie%20Romeinstraat%2028,%205803%20AE%20Venray&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
