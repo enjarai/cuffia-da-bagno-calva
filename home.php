fa518be943fb299706f44a8d6ae4eb25e445d61f
enjarai <enjarai123@gmail.com>
2022-03-16 13:49:53 +0100

    maak responsive met bootstrap

--- home.php
+++ home.php
@@ -1,46 +1,53 @@
+<?php
+include("src/check_included.php");
+check_included(__FILE__);
+?>
+
 <link rel='stylesheet' href='home.css' type='text/css'>
-<div class="grid-containerh">
 <div class="item1h">
 <!--    home-->
-    <h1 class="home">Benvenuti ospiti</h1>
-    <p class="info">
-        <br>
-        cuffia da bagno calva biedt de prachtige stranden van Maastricht aan.
-        Voor de kids is er een ballenbak waar zij lekker kunnen spelen met ballen!
-        Er zijn diverse snacks verkrijgbaar.
-        snackbar cuffia da bagno calva is een zeer geschikte locatie voor het vieren van een verjaardag, huwelijk of jubileum.
-        openingstijden ma - do 09:00 - 21:00 vr - zo 9:00 - 22:00
-        Tot snel bij Cuffia da bagno calva!
-        <br>
-    </p>
+    <figure class="text-center">
+        <h1 class="home">Benvenuti ospiti</h1>
+        <p class="info">
+            <br>
+            cuffia da bagno calva biedt de prachtige stranden van Maastricht aan.
+            Voor de kids is er een ballenbak waar zij lekker kunnen spelen met ballen!
+            Er zijn diverse snacks verkrijgbaar.
+            snackbar cuffia da bagno calva is een zeer geschikte locatie voor het vieren van een verjaardag, huwelijk of jubileum.
+            openingstijden ma - do 09:00 - 21:00 vr - zo 9:00 - 22:00
+            Tot snel bij Cuffia da bagno calva!
+            <br>
+        </p>
+    </figure>
 </div>
 <div class="item2h">
 <!--    picture-->
 </div>
-<div class="item3h">
+<div class="reviews container">
 <!--    review-->
-    <div class="sub-grid-containerh">
-        <div class="sub-grid-item1h">
+    <div class="row align-items-top justify-content-evenly">
+        <div class="col">
 <!--            r1-->
-            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
+            <img src="images/star2.jpg" alt="5" height="40px">
             <br>
             <p class="review">Top zaak goede kwaliteit en leuke medewerkers.</p>
             <br>
             <h5 class="home5">Han Bouma</h5>
         </div>
-        <div class="sub-grid-item2h">
+        <div class="col">
 <!--            r2-->
-            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
+            <img src="images/star2.jpg" alt="5" height="40px">
             <br>
             <p class="review">Snel, prima snacks en een goed gevuld bord. De patat is echt een aanrader.</p>
             <br>
             <h5 class="home5">Debby Lammers</h5>
         </div>
-        <div class="sub-grid-item3h">
+        <div class="col">
 <!--            r3-->
-            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
+            <img src="images/star2.jpg" alt="5" height="40px">
             <br>
             <p class="review">Heerlijk gegeten, vriendelijke bediening. Het zag er allemaal schoon en netjes uit. Wij komen er graag.</p>
+            <br>
             <h5 class="home5">Jan Dijnen</h5>
         </div>
 </div>
