395fdd2b4571f11d0007fd7cfb0ef260074a4db0
MilouSchipper <milou.schipper2004@gmail.com>
2022-03-16 12:49:08 +0100

    home done Milou

--- home.php
+++ home.php
@@ -1,2 +1,47 @@
-<div style="height: 100px" ></div>
-home
+<link rel='stylesheet' href='home.css' type='text/css'>
+<div class="grid-containerh">
+<div class="item1h">
+<!--    home-->
+    <h1 class="home">Benvenuti ospiti</h1>
+    <p class="info">
+        <br>
+        cuffia da bagno calva biedt de prachtige stranden van Maastricht aan.
+        Voor de kids is er een ballenbak waar zij lekker kunnen spelen met ballen!
+        Er zijn diverse snacks verkrijgbaar.
+        snackbar cuffia da bagno calva is een zeer geschikte locatie voor het vieren van een verjaardag, huwelijk of jubileum.
+        openingstijden ma - do 09:00 - 21:00 vr - zo 9:00 - 22:00
+        Tot snel bij Cuffia da bagno calva!
+        <br>
+    </p>
+</div>
+<div class="item2h">
+<!--    picture-->
+</div>
+<div class="item3h">
+<!--    review-->
+    <div class="sub-grid-containerh">
+        <div class="sub-grid-item1h">
+<!--            r1-->
+            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
+            <br>
+            <p class="review">Top zaak goede kwaliteit en leuke medewerkers.</p>
+            <br>
+            <h5 class="home5">Han Bouma</h5>
+        </div>
+        <div class="sub-grid-item2h">
+<!--            r2-->
+            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
+            <br>
+            <p class="review">Snel, prima snacks en een goed gevuld bord. De patat is echt een aanrader.</p>
+            <br>
+            <h5 class="home5">Debby Lammers</h5>
+        </div>
+        <div class="sub-grid-item3h">
+<!--            r3-->
+            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
+            <br>
+            <p class="review">Heerlijk gegeten, vriendelijke bediening. Het zag er allemaal schoon en netjes uit. Wij komen er graag.</p>
+            <h5 class="home5">Jan Dijnen</h5>
+        </div>
+</div>
+</div>
