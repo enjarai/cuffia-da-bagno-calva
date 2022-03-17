e6826de3b9b38fb00741b8a7fb86db54585955e3
kilian2705 <182624@novacollege.nl>
2022-03-17 13:43:13 +0100

    menukaart push2, Kilian

--- menukaart.php
+++ menukaart.php
@@ -1,31 +1,51 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/36dd471165.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="menukaart.css" type="text/css">
 
 
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <div class="menudropdown"
-            <select name="menucategorie">
+            <div class="dropdown">
+            <select class="dropdownmenu">
                 <option value="">Categorie 1</option>
                 <option value="">Categorie 2</option>
                 <option value="">Categorie 3</option>
                 <option value="">Categorie 4</option>
             </select>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-pen-to-square"></i>
+                <i class="fa-solid fa-trash"></i>
         </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-3">
-
+        <div class="col-3 colum" >
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
+            <dt>Product naam</dt>
         </div>
-        <div class="col-3">
-
+        <div class="col colum">
+            <dt>Beschrijving van product</dt>
         </div>
-        <div class="col-3">
-
+        <div class="col-md-auto colum">
+            <dt>72.33€</dt>
+            <dt>72.33€</dt>
+            <dt>72.33€</dt>
+            <dt>72.33€</dt>
+            <dt>72.33€</dt>
+            <dt>72.33€</dt>
         </div>
-        <div class="col-3">
-
+        <div class="col-md-auto colum">
+            <i class="fa-solid fa-pen-to-square"></i>
+            <i class="fa-solid fa-trash"></i>
         </div>
     </div>
 </div>
