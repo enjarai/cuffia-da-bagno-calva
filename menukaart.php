a9f0b574c16fa1800b565435d978b809b9f52aa3
kilian2705 <182624@novacollege.nl>
2022-03-17 09:39:13 +0100

    menukaart, Kilian

---
+++ menukaart.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="menukaart.css" type="text/css">
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="menudropdown"
+            <select name="menucategorie">
+                <option value="">Categorie 1</option>
+                <option value="">Categorie 2</option>
+                <option value="">Categorie 3</option>
+                <option value="">Categorie 4</option>
+            </select>
+        </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+    </div>
+</div>
