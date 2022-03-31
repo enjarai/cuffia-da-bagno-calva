cd4197a435570430a58936cc802c738b0241f7e1
kilian2705 <182624@novacollege.nl>
2022-03-31 11:27:02 +0200

    Home pagina responsive gemaakt, Kilian

--- home.php
+++ home.php
@@ -1,48 +1,44 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <link rel='stylesheet' href='home.css' type='text/css'>
-<div class="grid-containerh">
-<div class="item1h">
-<!--    home-->
-    <h1 class="home">Benvenuti ospiti</h1>
-    <p class="info">
-        <br>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col" id="col1">
+        <h1 class="home">Benvenuti ospiti</h1>
+        <p class="info">
         cuffia da bagno calva biedt de prachtige stranden van Maastricht aan.
-        Voor de kids is er een ballenbak waar zij lekker kunnen spelen met ballen!
-        Er zijn diverse snacks verkrijgbaar.
-        snackbar cuffia da bagno calva is een zeer geschikte locatie voor het vieren van een verjaardag, huwelijk of jubileum.
-        openingstijden ma - do 09:00 - 21:00 vr - zo 9:00 - 22:00
-        Tot snel bij Cuffia da bagno calva!
-        <br>
-    </p>
-</div>
-<div class="item2h">
-<!--    picture-->
-</div>
-<div class="item3h">
-<!--    review-->
-    <div class="sub-grid-containerh">
-        <div class="sub-grid-item1h">
-<!--            r1-->
-            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
-            <br>
-            <p class="review">Top zaak goede kwaliteit en leuke medewerkers.</p>
-            <br>
-            <h5 class="home5">Han Bouma</h5>
-        </div>
-        <div class="sub-grid-item2h">
-<!--            r2-->
-            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
-            <br>
-            <p class="review">Snel, prima snacks en een goed gevuld bord. De patat is echt een aanrader.</p>
-            <br>
-            <h5 class="home5">Debby Lammers</h5>
-        </div>
-        <div class="sub-grid-item3h">
-<!--            r3-->
-            <img src="images/star2.jpg" alt="5" height="40px" width="auto" >
-            <br>
-            <p class="review">Heerlijk gegeten, vriendelijke bediening. Het zag er allemaal schoon en netjes uit. Wij komen er graag.</p>
-            <h5 class="home5">Jan Dijnen</h5>
-        </div>
+        <br>Voor de kids is er een ballenbak waar zij lekker kunnen spelen met ballen!
+        <br>Er zijn diverse snacks verkrijgbaar.
+        <br>snackbar cuffia da bagno calva is een zeer geschikte locatie voor het vieren van een verjaardag, huwelijk of jubileum.
+        <br><br>openingstijden ma - do 09:00 - 21:00 vr - zo 9:00 - 22:00
+        <br><br>Tot snel bij Cuffia da bagno calva!
+        </p>
+    </div>
 </div>
 
+<div class="row row2">
+    <div class="col" id="col2">
+        <img src="images/hotdog.jpg">
+    </div>
 </div>
+
+        <div class="row row3">
+            <div class="col" id="col3">
+            <img src="images/star2.jpg">
+            <p>Top zaak goede kwaliteit en leuke medewerkers.</p>
+            <h5>Han Bouma</h5>
+        </div>
+            <div class="col colborder" id="col3">
+            <img src="images/star2.jpg">
+            <p>Snel, prima snacks en een goed gevuld bord. De patat is echt een aanrader.</p>
+            <h5>Debby Lammers</h5>
+        </div>
+            <div class="col" id="col3">
+            <img src="images/star2.jpg">
+            <p>Heerlijk gegeten, vriendelijke bediening. Het zag er allemaal schoon en netjes uit. Wij komen er graag.</p>
+            <h5>Jan Dijnen</h5>
+        </div>
+    </div>
+</div>
+
